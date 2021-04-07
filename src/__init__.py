5824b714e33c5ec45638d904db51d486a32b81e1
runoverPy <74095195+runoverPy@users.noreply.github.com>
2021-04-07 21:43:21 +0200

    can read ints

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,9 @@ from .Util import *
 from .Structures import Octree
 from .Visualiser import Display
 from .Reader import Reader
-from .parser import lexer
+from .parser import lexer, read, readbinary, translate, parse, Packet
+# import FileIO as io
+
 
 def zen():
     import this
